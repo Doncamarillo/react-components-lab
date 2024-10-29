65717861c3e4c6622ac048edfdbc56f42b37e301
doncamarillo <don_camarillo@yahoo.com>
2024-10-29 12:29:19 -0700

    added weather components + css

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import './WeatherForecast.css';
+
+
+const WeatherForecast = (props) => {
+  const {weatherForecast} = props
+  return (
+    <div className="weather">
+      <h2>{weatherForecast.day}</h2>
+      <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
+      <p><span>Conditions:</span> {weatherForecast.conditions}</p>
+      <p><span>Time:</span> {weatherForecast.time}</p>
+    </div>
+  );
+}
+
+export default WeatherForecast;
